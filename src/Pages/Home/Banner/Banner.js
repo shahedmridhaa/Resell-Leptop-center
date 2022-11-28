e4b5072343aa12788bfcb3ff4074e4641743b39e
shahedmridhaa <raselmridha9696@gmail.com>
2022-11-28 22:37:29 +0600

    check responsive

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,13 +3,15 @@ import bannerright from '../../../assest/bannerrightimg.png'
 
 const Banner = () => {
   return (
-    <div className="banner bg-slate-50 py-32 ">
+    <div className="banner bg-slate-50 py-32">
       <div className="container mx-auto">
-        <div className="w-full">
-          <div className=" flex flex-col justify-between lg:flex-row-reverse">
-            <img src={bannerright} alt="..." className="max-w-xl sm:mx-auto " />
-            <div className="sm:pt-16 mx-auto">
-              <h1 className="text-5xl font-bold">CHOOSE YOUR LEPTOP</h1>
+        <div className="w-full sm:px-3">
+          <div className=" flex flex-col justify-between items-center lg:flex-row-reverse sm:mx-auto">
+            <div>
+            <img src={bannerright} alt="..." className="max-w-xl sm:mx-auto w-96 lg:w-full" />
+            </div>
+            <div className="sm:pt-20">
+              <h1 className="text-4xl font-bold">CHOOSE YOUR LEPTOP</h1>
               <p className="py-6">
                 Looking for a good essay, research or speech topic on Laptop?
                 <br></br> ✓ Check our list of Here is different kind of leptop
